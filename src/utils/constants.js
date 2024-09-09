7ac6aa5f20045cbd39fa9ddb3a6a78959439fef1
Abhishek Yadav <130171935+ay1878768@users.noreply.github.com>
2024-09-10 01:19:36 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,6 @@ export const LOGO_URL="https://fabrx.co/preview/tastebite/assets/images/brands/b
 export const REST_IMAGE_URL="https://media-assets.swiggy.com/swiggy/image/upload/";
 
 export const RES_MENU_URL =
-    "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
+    "https://proxy.cors.sh/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
 
 export const ACCORDIAN_IMAGE_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
