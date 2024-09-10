8c0681e230f8b6453e8d06cfdddbff3e6354b8b4
Abhishek Yadav <130171935+ay1878768@users.noreply.github.com>
2024-09-10 19:45:22 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,6 @@ export const LOGO_URL="https://fabrx.co/preview/tastebite/assets/images/brands/b
 export const REST_IMAGE_URL="https://media-assets.swiggy.com/swiggy/image/upload/";
 
 export const RES_MENU_URL =
-    "https://proxy.cors.sh/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
+    "https://cors-handlers.vercel.app/api/?url=https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D28.6655181%26lng%3D77.258312%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING";
 
 export const ACCORDIAN_IMAGE_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
